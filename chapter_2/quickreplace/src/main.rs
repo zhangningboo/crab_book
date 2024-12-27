3d86ea760cbe61066634464f49eb7aed08b9fc94
zhangningboo <zhangningbo21@mails.ucas.ac.cn>
2024-12-27 10:02:21 +0800

    feat: 操作文件、文字颜色修改

--- chapter_2/quickreplace/src/main.rs
+++ chapter_2/quickreplace/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+use std::fs;
 use text_colorizer::*;
+use regex::Regex;
 
 
 #[derive(Debug)]  // 编译器生成额外的代码，可以直接打印：println!("{:?}", arguments)
@@ -32,9 +34,39 @@ fn parse_args() -> Arguments {
     }
 }
 
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target).unwrap();
+    Ok(
+        regex.replace_all(text, replacement).to_string()
+    )
+}
+
 fn main() {
     let args = parse_args();
     println!("{:?}", args);
-}
 
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to read from file '{}': {}", "Error:".red().bold(), args.filename.red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to write to file '{}': {}", "Error:".red().bold(), args.output.red().bold(), e);
+            std::process::exit(1);
+        }
+    }
+}
 
